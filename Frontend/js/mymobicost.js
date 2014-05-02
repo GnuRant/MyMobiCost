6da0b8cfb3d94c71dbadec5abf1e5f8e76ce21fa
Luca D'Incà <luca.dinca@gmail.com>
2014-05-02 17:22:36 +0200

    Aggiunto controllo per mostrare on non mostrare il benvenuto

--- Frontend/js/mymobicost.js
+++ Frontend/js/mymobicost.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-	
+	/*Constrollo se è la prima visita dell'utente o se ci sono
+		già i dati salvati in localstorage 
+	*/
+	if (true) {
+		//Se e la prima visita carico il menu di benvenuto,
+		//di default nascosto
+		$("#welcome").show();
+	}else{
+		//Carico i dati dell'utente
+	}
 });
