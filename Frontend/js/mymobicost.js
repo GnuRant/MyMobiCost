d2d5ce0c374c58fdcf825d595be70facecbecbf0
Luca D'Incà <luca.dinca@gmail.com>
2014-05-02 17:31:02 +0200

    Se il benvenuto è già presente non lomostro nuovamente

--- Frontend/js/mymobicost.js
+++ Frontend/js/mymobicost.js
@@ -2,11 +2,14 @@ $(document).ready(function(){
 	/*Constrollo se è la prima visita dell'utente o se ci sono
 		già i dati salvati in localstorage 
 	*/
-	if (true) {
+	if (false) {
 		//Se e la prima visita carico il menu di benvenuto,
 		//di default nascosto
-		$("#welcome").show();
+		if ($("#welcome").css("display") == "none") {
+			$("#welcome").show();
+		};
 	}else{
 		//Carico i dati dell'utente
 	}
 });
+
