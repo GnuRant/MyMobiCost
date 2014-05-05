acefaf752fdc07b28851e96016a4bfae7f990125
Juleship <michelon.giulio@gmail.com>
2014-05-05 14:13:18 +0200

    non so fare js ma comunque adesso vanno i moduli

--- Frontend/js/mymobicost.js
+++ Frontend/js/mymobicost.js
@@ -16,12 +16,18 @@ $(document).ready(function(){
 	//==========================  UI ================================
 	//===============================================================
 
-	// rende i select meravigliosi colorati e hipster
-	$("select").selectpicker({style: 'btn-hg btn-primary', menuStyle: 'dropdown-inverse'});
-  	$("button.dropdown-toggle").css({
-  	    'background':'#404040'
-  	});
-	
+		// rende i select meravigliosi colorati e hipster
+		$("select").selectpicker({style: 'btn-hg btn-primary', menuStyle: 'dropdown-inverse'});
+	  	$("button.dropdown-toggle").css({
+	  	    'background':'#404040'
+	  	});
+		
+	  $('#new-session').click(function () {
+	  	$.get( "partials/famiglia.html", function( data ) {
+			  $( "#form-container" ).html( data );
+			});
+	  });
+
   	// switch costo abitazione (conosciuto o no)
   	$('#checkbox-abitazione').change(function () {
   	  $('#costo-sconosciuto').fadeToggle();
