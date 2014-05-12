4d7dbdb7e6351d79736e50bdcec50290b2507a98
Luca D'Incà <luca.dinca@gmail.com>
2014-05-12 13:34:17 +0200

    Animazione cool

--- Frontend/js/mymobicost-ui.js
+++ Frontend/js/mymobicost-ui.js
@@ -1067,7 +1067,7 @@ function create_chart(data) {
 
   var options = {
     animationSteps : 70,
-    animationEasing : "easeOutBounce",
+    animationEasing : "easeInOutQuart",
     animateScale : false,
     segmentShowStroke: true,
     segmentStrokeColor: "#ECF0F1"
